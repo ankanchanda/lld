93e6340c8b5066e7634547d31b604613b750e71c
ankanchanda <ankan.chanda74@gmail.com>
2024-02-08 21:05:49 +0530

    separated oop

--- oop/abstraction/Main.java
+++
@@ -1,9 +0,0 @@
-package lld.oop.abstraction;
-
-public class Main {
-    public static void main(String[] args) {
-        Emu emu = new Emu();
-        System.out.println(emu.getName());
-        emu.attack();
-    }
-}
