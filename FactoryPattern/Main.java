06c811cebe8fda4ff9021b6120b488fe1f6fa6de
Ankan Chanda <ankanchanda@Ankans-MacBook-Air.local>
2024-05-02 09:37:14 +0530

    factory design pattern

---
+++ FactoryPattern/Main.java
@@ -0,0 +1,9 @@
+package lld.FactoryPattern;
+
+public class Main {
+    public static void main(String[] args) {
+        ShapeFactory shapeFactory = new ShapeFactory();
+        Shape shapeObj = shapeFactory.getShape("CIRCLE");
+        shapeObj.draw();
+    }
+}
---
+++ FactoryPattern/ShapeFactory.java
@@ -0,0 +1,17 @@
+package lld.FactoryPattern;
+
+public class ShapeFactory {
+    
+    public Shape getShape(String input){
+
+        switch (input) {
+            case "CIRCLE":
+                return new Circle();
+            case "RECTANGLE":
+                return new Rectangle();
+            default:
+                return null;
+        }
+
+    }
+}
